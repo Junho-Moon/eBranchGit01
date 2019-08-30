da041f1a0188297607b45a70abb90f7758491c3e
sist <sist@DESKTOP-SVEATCF>
2019-08-30 15:46:11 +0900

    hotfix

--- eBranchGit01/src/eBranchGit01/Hello.java
+++ eBranchGit01/src/eBranchGit01/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
 	
 	public static void main(String arg[]) {
 		System.out.println("Hello Wolrd!");
+		System.out.println("Hello Wolrd!");
+		System.out.println("Hello Wolrd!");
 	}
 }
